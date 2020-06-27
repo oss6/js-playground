80dad5fb48d563e66d3e2eeaa3445d62152250b2
oss6 <ossedb@gmail.com>
2020-06-28 00:27:05 +0100

    Add proxy change notification use case

---
+++ proxy/change-notification.js
@@ -0,0 +1,12 @@
+function observe(obj, callback) {
+  return new Proxy(obj, {
+    set(target, property, value) {
+      callback(property, value);
+      target[property] = value;
+    }
+  });
+}
+
+const x = { name: 'John' };
+const p = observe(x, (property, value) => console.log(property, value));
+p.name = 'Gianni';
